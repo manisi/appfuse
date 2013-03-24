b0032f1ae14f5e251c92a61180c7794acd5d69a0
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2013-03-24 21:11:27 +0100

    Person CRUD user interface

---
+++ web/gwt/src/main/java/org/appfuse/webapp/client/ui/people/list/PeopleSearchView.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.appfuse.webapp.client.ui.people.list;
+
+import org.appfuse.webapp.client.application.base.view.ProxySearchView;
+import org.appfuse.webapp.client.proxies.PersonProxy;
+
+/**
+ * @author ivangsa
+ *
+ */
+public interface PeopleSearchView extends ProxySearchView<PersonProxy, String> {
+
+
+}
